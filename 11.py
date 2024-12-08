aec03007b31cddbee472912467f085f94be69810
NoobScience <noobscience123@gmail.com>
2024-12-08 17:03:51 +0530

    Add 11

---
+++ 11.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+
+data=pd.read_csv('D:\soujanya\Machine Learning 22-23 I Sem\Machine Learning 2022-23 IIsem\ML LAB\Datasets\diabetes.csv',names=names)
+
+print(data)
+
+X =data.drop('class',axis=1)
+y = data['class']
+
+from sklearn.model_selection import train_test_split # implementing train-testsplit
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=66)
+from sklearn import model_selection
+
+rfc =RandomForestClassifier()
+rfc.fit(X_train,y_train)
+
+rfc_predict = rfc.predict(X_test)
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report,confusion_matrix
+rfc_cv_score = cross_val_score(rfc, X, y, cv=10, scoring='roc_auc')
+print("=== Confusion Matrix ===")
+print(confusion_matrix(y_test,rfc_predict))
+print('\n')
+print("=== Classification Report ===")
+print(classification_report(y_test, rfc_predict))
+print('\n')
+print("=== All AUC Scores===")
+print(rfc_cv_score)
+print('\n')
+print("=== Mean AUC Score ===")
+print("Mean AUC Score - Random Forest: ", rfc_cv_score.mean())
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import GradientBoostingRegressor
+col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+pima = pd.read_csv("D:\soujanya\Machine Learning 22-23 I Sem\Machine Learning 2022-23 II sem\ML LAB\Datasets/diabetes.csv", header=None, names=col_names)
+print(pima)
+feature_cols = ['pregnant', 'insulin', 'bmi', 'age','glucose','bp']
+X = pima[feature_cols]
+y = pima.label
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20) # initializing the boosting
+module with default parameters
+model = GradientBoostingRegressor()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+from sklearn.metrics import r2_score
+print('score==',r2_score(y_test,y_pred))
+print('mean_sqrd_error is==',mean_squared_error(y_test,y_pred))
+print('root_mean_squared error of is==',np.sqrt(mean_squared_error(y_test,y_pred)))
